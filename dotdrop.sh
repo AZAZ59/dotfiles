6ce624a0e145d4d4647cb7261a9c9d183d4df1bb
AZAZ59 <step-srg@yandex.ru>
2017-03-28 15:59:18 +0400

    add files

---
+++ dotdrop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# author: deadc0de6 (https://github.com/deadc0de6)
+# Copyright (c) 2017, deadc0de6
+
+args="$@"
+cur=`dirname $(readlink -f $0)`
+opwd=`pwd`
+bin="${cur}/dotdrop/dotdrop/dotdrop.py"
+
+# run dotdrop
+cfg="${cur}/config.yaml"
+cd ${cur}
+python3 ${bin} --cfg=${cfg} $args
+cd ${opwd}
